1b2a849a2184937d61cd55b5c4f1bbbe4a080130
siraajul <shirajulislamparvez@gmail.com>
2024-12-11 23:33:00 +0600

    update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import React from "react";
+import { Button } from "@nextui-org/button";
+import { Calendar } from "@nextui-org/calendar";
+import { today, getLocalTimeZone } from "@internationalized/date";
 
 const HomePage = () => {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div className=" flex items-center justify-center">
+      <div className=" justify-start items-center ">
+        <Button className="" color="primary">
+          Button
+        </Button>
+      </div>
+      <div className=" bg-slate-600 ">
+        <Calendar
+          isReadOnly
+          aria-label="Date (Read Only)"
+          value={today(getLocalTimeZone())}
+        />
+      </div>
+    </div>
   );
 };
 
